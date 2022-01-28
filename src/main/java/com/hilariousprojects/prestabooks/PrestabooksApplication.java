7a5c6879e4788aa094c66b46b76cf5ab5b9659ce
fernandohruz <fernandohruz@hotmail.com>
2022-01-27 19:48:06 -0600

    Add files generated with spring initializr

---
+++ src/main/java/com/hilariousprojects/prestabooks/PrestabooksApplication.java
@@ -0,0 +1,13 @@
+package com.hilariousprojects.prestabooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestabooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestabooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hilariousprojects/prestabooks/PrestabooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilariousprojects.prestabooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestabooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
