bb9c90c75c532e44cf1413259847175b9cad29b5
fernandohruz <fernandohruz@hotmail.com>
2022-01-28 22:22:57 -0600

    write findBy for each model

--- src/main/java/com/hilariousprojects/prestabooks/repo/BookRepo.java
+++ src/main/java/com/hilariousprojects/prestabooks/repo/BookRepo.java
@@ -1,2 +1,8 @@
-package com.hilariousprojects.prestabooks.repo;public interface BookRepo {
+package com.hilariousprojects.prestabooks.repo;
+
+import com.hilariousprojects.prestabooks.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepo extends JpaRepository<Book, Long> {
+    Book findByTitle(String title);
 }
--- src/main/java/com/hilariousprojects/prestabooks/repo/RoleRepo.java
+++ src/main/java/com/hilariousprojects/prestabooks/repo/RoleRepo.java
@@ -1,2 +1,8 @@
-package com.hilariousprojects.prestabooks.repo;public interface RoleRepo {
+package com.hilariousprojects.prestabooks.repo;
+
+import com.hilariousprojects.prestabooks.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role,Long> {
+    Role findByName(String name);
 }
