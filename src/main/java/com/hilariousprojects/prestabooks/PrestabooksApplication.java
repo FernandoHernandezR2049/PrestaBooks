1e1e19ccd05c9f860667efd8b5d8961a80b45ed7
fernandohruz <fernandohruz@hotmail.com>
2022-02-02 21:30:14 -0600

    Add users, roles, and assigned some roles to users to test if the userService methods are working

--- src/main/java/com/hilariousprojects/prestabooks/PrestabooksApplication.java
+++ src/main/java/com/hilariousprojects/prestabooks/PrestabooksApplication.java
@@ -1,7 +1,14 @@
 package com.hilariousprojects.prestabooks;
 
+import com.hilariousprojects.prestabooks.domain.Role;
+import com.hilariousprojects.prestabooks.domain.User;
+import com.hilariousprojects.prestabooks.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class PrestabooksApplication {
@@ -9,5 +16,25 @@ public class PrestabooksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PrestabooksApplication.class, args);
 	}
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_LIBRARIAN"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
+			userService.saveUser(new User(null,"Dwight S","DelPuebloLibrary","123", new ArrayList<>()));
+			userService.saveUser(new User(null,"Jim H","NewWorldLibrary","123", new ArrayList<>()));
+			userService.saveUser(new User(null,"Mike Scott","LibAdmin","123", new ArrayList<>()));
+			userService.saveUser(new User(null,"Andy B","LibUser1","123", new ArrayList<>()));
+
+			userService.addRoleToUser("LibUser1","ROLE_USER");
+			userService.addRoleToUser("DelPuebloLibrary","ROLE_LIBRARIAN");
+			userService.addRoleToUser("NewWorldLibrary","ROLE_LIBRARIAN");
+			userService.addRoleToUser("LibAdmin","ROLE_ADMIN");
+			userService.addRoleToUser("LibAdmin","ROLE_SUPER_ADMIN");
+
+		};
+	}
 }
