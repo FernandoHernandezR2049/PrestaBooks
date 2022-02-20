aa2ece5f5750856f2512cf8e7878a77ffa6e9a5b
fernandohruz <fernandohruz@hotmail.com>
2022-02-19 21:25:43 -0600

    Add: Code for saving new books into the database

--- src/main/java/com/hilariousprojects/prestabooks/PrestabooksApplication.java
+++ src/main/java/com/hilariousprojects/prestabooks/PrestabooksApplication.java
@@ -1,7 +1,9 @@
 package com.hilariousprojects.prestabooks;
 
+import com.hilariousprojects.prestabooks.domain.Book;
 import com.hilariousprojects.prestabooks.domain.Role;
 import com.hilariousprojects.prestabooks.domain.User;
+import com.hilariousprojects.prestabooks.service.BookService;
 import com.hilariousprojects.prestabooks.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -25,7 +27,7 @@ public class PrestabooksApplication {
 	}
 
 	@Bean
-	CommandLineRunner run(UserService userService){
+	CommandLineRunner run(UserService userService, BookService bookService){
 		return args -> {
 			userService.saveRole(new Role(null, "ROLE_USER"));
 			userService.saveRole(new Role(null, "ROLE_LIBRARIAN"));
@@ -36,6 +38,11 @@ public class PrestabooksApplication {
 			userService.saveUser(new User(null,"Jim H","NewWorldLibrary","123", new ArrayList<>()));
 			userService.saveUser(new User(null,"Mike Scott","LibAdmin","123", new ArrayList<>()));
 			userService.saveUser(new User(null,"Andy B","LibUser1","123", new ArrayList<>()));
+
+
+			bookService.saveBook(new Book(null, "Clean Code", "Robert C. Martin", "234h235",1));
+			bookService.saveBook(new Book(null, "Dune","Frank Herbert","sdfa567dsf",1));
+			bookService.saveBook(new Book(null, "The Left Hand Of Darkness","Ursula K. Le Guin","asdf5895fds",0));
 
 			userService.addRoleToUser("LibUser1","ROLE_USER");
 			userService.addRoleToUser("DelPuebloLibrary","ROLE_LIBRARIAN");
