3ae8104ba3eecee5ef5bec32c770eee10b5b6e09
fernandohruz <fernandohruz@hotmail.com>
2022-02-20 18:25:56 -0600

    Add: Method for removing a role from the user.

--- src/main/java/com/hilariousprojects/prestabooks/service/UserService.java
+++ src/main/java/com/hilariousprojects/prestabooks/service/UserService.java
@@ -9,6 +9,7 @@ public interface UserService {
     User saveUser(User user);
     Role saveRole(Role role);
     void addRoleToUser(String username, String roleName);
+    void removeRoleFromUser(String username, String roleName);
     User getUser(String username);
     List<User>getUsers();
 }
