9ba704a91366029ecca602718b140efbf0d6ca70
Fernando Hernandez <fernandohruz@hotmail.com>
2022-05-10 14:51:38 -0500

    modified location of some files

--- src/main/java/com/hilariousprojects/prestabooks/security/SecurityConfig.java
+++ src/main/java/com/hilariousprojects/prestabooks/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
